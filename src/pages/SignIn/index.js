65aa05e014fe77d4b0455142b358f7f7f29190c1
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-12-23 23:34:32 -0300

    chore: input component + button component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -3,12 +3,18 @@ import { Text } from 'react-native';
 
 // import { Container } from './styles';
 
-import Background from '~/components/Background';
+import Input from '~/components/Input/index';
+import Button from '~/components/Button/index';
+import Background from '~/components/Background/index';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input style={{ marginTop: 30 }} icon="call" placeholder="Digite aqui" />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
