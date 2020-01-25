194c9d74546704ed2ded653aa1274a085bc4ba68
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-01-24 23:47:43 -0300

    chore: navigation options on react-navigation-stack

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -29,6 +32,17 @@ export default (signedIn = false) =>
                 SelectDateTime,
                 Confirm,
               }),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
             },
             Profile,
           },
