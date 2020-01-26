2bb534fbd5069463e1010daef4736b370c5476e0
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-01-25 21:57:07 -0300

    chore: styling SelectProvider screen

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Proptypes from 'prop-types';
 import { TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
+
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
-export default function SelectProvider() {
-  return <Background />;
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item }) => (
+            <Provider
+              onPress={() => navigation.navigate('SelectDateTime', { item })}
+            >
+              <Avatar
+                source={{
+                  uri: item.avatar
+                    ? `${api.defaults.baseURL}/files/${item.avatar.path}`
+                    : `https://api.adorable.io/avatars/50/${item.name}.png`,
+                }}
+              />
+              <Name>{item.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
@@ -23,3 +61,9 @@ SelectProvider.navigationOptions = ({ navigation }) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectProvider.propTypes = {
+  navigation: Proptypes.shape({
+    navigate: Proptypes.func.isRequired,
+  }).isRequired,
+};
